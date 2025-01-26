17b2c35696e6658692e4571c608249618e9117a5
Ondayo <otienosamueloduor@gmail.com>
2025-01-26 21:41:57 +0300

    Modified fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -10,21 +10,19 @@ async function fetchUserData() {
 
     const users = await response.json();
 
-    dataConatainer.innerHTML = '';
+    dataContainer.innerHTML = "";
 
     const userList = document.createElement("ul");
-    users.forEach(user => {
+    users.forEach((user) => {
       const listItem = document.createElement("li");
       listItem.textContent = user.name;
       userList.appendChild(listItem);
     });
 
-    dataConatainer.appendChild(userList);
-
+    dataContainer.appendChild(userList);
   } catch {
-
-    dataConatainer.innerHTML = '';
-    dataConatainer.textContent = "Failed to load user data.";
+    dataContainer.innerHTML = "";
+    dataContainer.textContent = "Failed to load user data.";
     console.error("Error fetching user data:", error);
   }
 }
